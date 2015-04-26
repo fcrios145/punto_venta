9ed8dc8f0eb6b0ef5250677b822142da138a765f
fcrios145 <fcrios145@hotmail.com>
2015-04-25 23:53:57 -0600

    se cambia de ruta el settings y se acomoda la ruta en el manage

--- template/settings/dev.py
+++ template/settings/dev.py
@@ -52,6 +52,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'template.urls'
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -84,14 +85,6 @@ WSGI_APPLICATION = 'template.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
