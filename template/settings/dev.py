fdf40a879c9feeb83d8a52b12d8e7f025308152a
fcrios145 <fcrios145@hotmail.com>
2015-04-26 00:09:21 -0600

    se configura static root

--- template/settings/dev.py
+++ template/settings/dev.py
@@ -104,3 +104,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/webapps/glucosetracker/static/'
+
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
